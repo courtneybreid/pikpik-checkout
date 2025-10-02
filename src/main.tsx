f44806923b498e82b89515dcebe42645cba10a28
Courtney B Reid <courtneybreid@gmail.com>
2025-10-02 17:28:43 -0400

    Create test Apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
+import { ApolloProvider } from "@apollo/client/react";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const client = new ApolloClient({
+  link: new HttpLink({uri: 'https://flyby-router-demo.herokuapp.com/'}),
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
